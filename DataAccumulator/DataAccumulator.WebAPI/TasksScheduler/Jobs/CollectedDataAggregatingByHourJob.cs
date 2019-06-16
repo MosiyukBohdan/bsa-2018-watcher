dabc8abf7f75bb287a4ca24bb749aa0ee26c186a
Mosiyuk Bohdan <bmosiyuk@gmail.com>
2019-06-16 17:02:04 +0300

    Add logger to hourly job

--- DataAccumulator/DataAccumulator.WebAPI/TasksScheduler/Jobs/CollectedDataAggregatingByHourJob.cs
+++ DataAccumulator/DataAccumulator.WebAPI/TasksScheduler/Jobs/CollectedDataAggregatingByHourJob.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using DataAccumulator.DataAggregator.Interfaces;
 using DataAccumulator.Shared.Models;
+using Microsoft.Extensions.Logging;
 using Quartz;
 
 namespace DataAccumulator.WebAPI.TasksScheduler.Jobs
@@ -9,15 +10,22 @@ namespace DataAccumulator.WebAPI.TasksScheduler.Jobs
     public class CollectedDataAggregatingByHourJob : IJob
     {
         private readonly IDataAggregatorCore<CollectedDataDto> _dataAggregatorCore;
-        public CollectedDataAggregatingByHourJob(IDataAggregatorCore<CollectedDataDto> dataAggregatorCore)
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger<CollectedDataAggregatingByHourJob> _logger;
+
+        public CollectedDataAggregatingByHourJob(IDataAggregatorCore<CollectedDataDto> dataAggregatorCore, 
+            ILoggerFactory loggerFactory)
         {
             _dataAggregatorCore = dataAggregatorCore;
+            _loggerFactory = loggerFactory;
+            _logger = loggerFactory?.CreateLogger<CollectedDataAggregatingByHourJob>() ?? throw new ArgumentNullException(nameof(loggerFactory));
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
             try
             {
+                _logger.LogError($"CollectedDataAggregatingByHourJob start");
                 var sourceType = CollectedDataType.Accumulation;
                 var destinationType = CollectedDataType.AggregationForHour;
                 var timeSpan = TimeSpan.FromHours(1);
@@ -29,6 +37,7 @@ namespace DataAccumulator.WebAPI.TasksScheduler.Jobs
             catch (Exception e)
             {
                 Console.WriteLine(e);
+                _logger.LogError($"CollectedDataAggregatingByHourJob exception {e}");
                 throw;
             }
         }
